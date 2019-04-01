a69b446ff66fd84c473a71439ddc42b5a605699f
nikhil1328 <31434730+nikhil1328@users.noreply.github.com>
2019-04-01 14:05:55 +0530

    Create Alphabets.py

---
+++ Alphabets.py
@@ -0,0 +1,107 @@
+#1st April 2019
+#Wirtten By NIKHIL KULKARNI
+#Use case: This script is creted to developed fun and enjoyment in kids while learning ABCD.
+
+
+xx= input("enter anything: ")
+
+if xx=='a' or xx=="A":
+    print ("A for APPLE")
+
+elif xx=='b'or xx=="B":
+    print ("B for BALL")
+
+elif xx=="c" or xx=="C":
+    print("C for CAT")
+
+elif xx=="d" or xx=="D":
+    print("D for Dog")
+
+elif xx=="e" or xx=="E":
+    print("E for ELEPHANT")
+
+elif xx=="f" or xx=="F":
+    print("F for FISH")
+
+elif xx == "g" or xx == "G":
+    print("G for GUN")
+
+
+elif xx=="H" or xx=="h":
+    print("H for HAT")
+
+
+elif xx=="I" or xx=="i":
+    print("I for INDIA")
+
+elif xx == "j" or xx == "J":
+    print("J for JOCKER")
+
+
+elif xx=="K" or xx=="k":
+    print("K for KING")
+
+
+elif xx=="l" or xx=="L":
+    print("L for LION")
+
+
+elif xx=="m" or xx=="M":
+    print("M for MOTHER")
+
+
+elif xx=="n" or xx=="N":
+    print("N for NUN")
+
+
+elif xx=="o" or xx=="O":
+    print("O for ORANGE")
+
+
+elif xx=="p" or xx=="P":
+    print("P for PARROT")
+
+
+elif xx=="q" or xx=="Q":
+    print("Q for QUEEN")
+
+
+elif xx=="r" or xx=="R":
+    print("R for RAT")
+
+
+elif xx=="s" or xx=="S":
+    print("s for SUN")
+
+
+elif xx=="t" or xx=="T":
+    print("T for TIGER")
+
+
+elif xx=="u" or xx=="U":
+    print("U for UMBRELLA")
+
+
+elif xx=="v" or xx=="V":
+    print("V for VAN")
+
+
+elif xx=="w" or xx=="W":
+    print("W for WAN")
+
+
+elif xx=="x" or xx=="X":
+    print("X for XEROX")
+
+
+elif xx=="y" or xx=="Y":
+    print("Y for YELLOW")
+
+
+elif xx=="z" or xx=="Z":
+    print("Z for ZEBRA")
+
+else:
+    print(" Try Again")
+
+
